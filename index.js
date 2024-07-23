f92306238619fb55cea30e74a95018524f82f7f1
yillkid <yillkid@gmail.com>
2024-07-23 23:28:50 +0800

    post

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var app = express();
 
+// 使用 express.json() 中間件來解析 JSON 格式的請求體
+app.use(express.json());
+
 // 根路徑的 GET 請求處理
 app.get('/', function(req, res) {
   res.send('<h1>Hello</h1> Express');
 });
 
-// /data 路徑的 GET 請求處理，帶查詢參數
-app.get('/data', function(req, res) {
-  // 獲取查詢參數
-  const name = req.query.name || 'Guest';
+// /data 路徑的 POST 請求處理，接收請求體中的 JSON 數據
+app.post('/data', function(req, res) {
+  // 獲取請求體中的數據
+  const name = req.body.name || 'Guest';
   
   // 準備回應數據
   const data = {
